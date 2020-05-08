d2441c2c2e3b97d95c6bbd46cdf7fe6426a24849
Odumogwu Ezekiel <ezek4real2013@gmail.com>
2020-05-08 22:49:47 +0100

    PWA
    making a progresive web app

--- calculator.js
+++ calculator.js
@@ -1,3 +1,22 @@
+			// This is the "Offline page" service worker
+			// Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
+			// Check compatibility for the browser we're running this in
+			if ("serviceWorker" in navigator) {
+			  if (navigator.serviceWorker.controller) {
+			    console.log("[PWA Builder] active service worker found, no need to register");
+			  } else {
+			    // Register the service worker
+			    navigator.serviceWorker
+			      .register("pwabuilder-sw.js", {
+			        scope: "./"
+			      })
+			      .then(function (reg) {
+			        console.log("[PWA Builder] Service worker has been registered for scope: " + reg.scope);
+			      });
+			  }
+			}
+//End of webWalker
+
 			function numberWithCommas(x) {
 				var parts = x.toString().split(".");
 				parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ","); 
